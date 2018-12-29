2f067b95ff3d93bcb786369438ea2129d08c0ab5
Thiago Ventura <thiventura@gmail.com>
2018-12-29 11:50:45 -0300

    Testing Augmentor and folders renaming

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from google_images_download import google_images_download
+import Augmentor
+import os.path
 
 # Download images
 response = google_images_download.googleimagesdownload()
@@ -10,7 +12,8 @@ color_type = "full-color"
 size = "large"
 offset = 1
 print_urls = True
-output_directory = "dataset"
+output_directory = os.path.join("dataset", "download")
+image_directory = "charmander-3d"
 
 #creating list of arguments
 arguments = {
@@ -21,8 +24,20 @@ arguments = {
     "size": size,
     "offset": offset,
     "print_urls": print_urls,
-    "output_directory": output_directory
+    "output_directory": output_directory,
+    "image_directory": image_directory
 }
 
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
+
+
+# Data augmentation
+p = Augmentor.Pipeline(
+    os.path.join(output_directory, image_directory),
+    os.path.join("..", "..", "augmented", image_directory) )
+
+p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
+p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
+p.sample(10)
+p.process()
