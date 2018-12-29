ec49670cb2878e7efd35a89b71cee623dd2feb23
Thiago Ventura <thiventura@gmail.com>
2018-12-29 13:25:43 -0300

    Reading list of classes from file and processing each one

--- main.py
+++ main.py
@@ -2,42 +2,39 @@ from google_images_download import google_images_download
 import Augmentor
 import os.path
 
-# Download images
-response = google_images_download.googleimagesdownload()
 
-keywords = "pokemon charmander 3d"
-limit = 2
-format_image = "jpg"
-color_type = "full-color"
-size = "large"
-offset = 1
-print_urls = True
+DOWNLOAD_QUANTITY_PER_CLASS = 2
+AUGMENTATION_QUANTITY_IMAGES = 10
 output_directory = os.path.join("dataset", "download")
-image_directory = "charmander-3d"
-
-#creating list of arguments
-arguments = {
-    "keywords": keywords,
-    "limit": limit,
-    "format": format_image,
-    "color_type": color_type,
-    "size": size,
-    "offset": offset,
-    "print_urls": print_urls,
-    "output_directory": output_directory,
-    "image_directory": image_directory
-}
-
-paths = response.download(arguments)   #passing the arguments to the function
-print(paths)   #printing absolute paths of the downloaded images
 
 
-# Data augmentation
-p = Augmentor.Pipeline(
-    os.path.join(output_directory, image_directory),
-    os.path.join("..", "..", "augmented", image_directory) )
+# Load classes from CSV
+classesFileName = os.path.join("samples", "pokemon.csv")
+classesFile = open(classesFileName, "r")
+for classData in classesFile:
+    className, classKeywords = classData.split(",")
 
-p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
-p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
-p.sample(10)
-p.process()
+    # Download images
+    response = google_images_download.googleimagesdownload()
+    offset = 1
+    arguments = {
+        "keywords": classKeywords,
+        "limit": DOWNLOAD_QUANTITY_PER_CLASS,
+        "format": "jpg",
+        "color_type": "full-color",
+        "size": "large",
+        "offset": offset,
+        "print_urls": True,
+        "output_directory": output_directory,
+        "image_directory": className
+    }
+    response.download(arguments)
+
+    # Data augmentation
+    p = Augmentor.Pipeline(
+        os.path.join(output_directory, className),
+        os.path.join("..", "..", "augmented", className) )
+    p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
+    p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
+    p.sample(AUGMENTATION_QUANTITY_IMAGES)
+    p.process()
